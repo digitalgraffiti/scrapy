02d1d7a683f87d118f2ddaf216e8144fde60d56d
Meg Waring <meg@digitalgraffiti.ca>
2016-09-18 00:21:35 -0700

    update

--- blgenre/genre/genre/items.py
+++ blgenre/genre/genre/items.py
@@ -1,14 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class GenreItem(scrapy.Item):
-    gname = scrapy.Field()
-    gdesc = scrapy.Field()
-    glink = scrapy.Field()
+    name = scrapy.Field()
+    url = scrapy.Field()
+    desc = scrapy.Field()
--- blgenre/genre/genre/spiders/genre_spider.py
+++ blgenre/genre/genre/spiders/genre_spider.py
@@ -1,16 +1,18 @@
 import scrapy
+from genre.items import GenreItem
 
 
 class GenreSpider(scrapy.Spider):
-    name = "genre"
-    start_urls = [
-        'https://en.wikipedia.org/wiki/List_of_writing_genres',
-    ]
+    name = 'genres-wiki'
+    allowed_domains = ['en.wikipedia.org']
+    start_urls = ['https://en.wikipedia.org/wiki/List_of_writing_genres']
 
     def parse(self, response):
-        for genre in response.xpath('//ul/li'):
-            gname = genre.xpath('//ul/li/a/text()').extract_first()
-            glink = genre.xpath('//ul/li/a').extract_first()
-            gdesc = genre.xpath('//ul/li/text()').extract_first()
-            print(u'{}: {}'.format(gname, glink, gdesc))
+        # first we get the links from the main genre section
+        links = response.xpath('//*[@id="mw-content-text"]/ul[1]/li/a')
+        for link in links:
+            item = GenreItem()
+            item['url'] = response.urljoin(link.xpath('@href').extract_first())
+            item['name'] = link.xpath('@title').extract_first()
+            yield item
 
