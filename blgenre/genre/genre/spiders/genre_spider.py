cf8cef1933e709927cdd2d71c466fa8494fd3aa9
Meg Waring <meg@digitalgraffiti.ca>
2016-09-18 12:41:46 -0700

    added .extract_first() as a guess, but i don't know because i don't understand the error I'm getting

--- blgenre/genre/genre/spiders/genre_spider.py
+++ blgenre/genre/genre/spiders/genre_spider.py
@@ -13,7 +13,7 @@ class GenreSpider(scrapy.Spider):
 # Major genres
         for link in response.xpath('//*[@id="mw-content-text"]/ul[1]/li/a'):
             item = GenreItem()
-            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[1]') #<---I give up. I just want the text. this is to be the same on every item in this particular list
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[1]').extract_first() #<---I give up. I just want the text. this is to be the same on every item in this particular list
             item['url'] = response.urljoin(link.xpath('@href').extract_first())
             item['name'] = link.xpath('@title').extract_first()
             yield item
@@ -22,7 +22,7 @@ class GenreSpider(scrapy.Spider):
 # Common genres: fiction
         for link in response.xpath('//*[@id="mw-content-text"]/ul[2]/li'):
             item = GenreItem()
-            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[2]') #<---I give up
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[2]').extract_first() #<---I give up
             item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
             item['name'] = link.xpath('a/@title').extract_first()# some of these dont have links and it mucks stuff up
             item['desc'] = link.xpath('text()').extract_first()# the name ends up here with gibberish characters between it and the desc
@@ -32,7 +32,7 @@ class GenreSpider(scrapy.Spider):
 # Common genres: nonfiction
         for link in response.xpath('//*[@id="mw-content-text"]/ul[3]/li'):
             item = GenreItem()
-            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[3]') #<---I give up
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[3]').extract_first() #<---I give up
             item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
             item['name'] = link.xpath('a/@title').extract_first()
             item['desc'] = link.xpath('text()').extract_first()
@@ -42,7 +42,7 @@ class GenreSpider(scrapy.Spider):
 # Genres and subgenres
         for link in response.xpath('//*[@id="mw-content-text"]/ul[3]/li'):# ther are lots and lots of sub-lists
             item = GenreItem()
-            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h2[3]') #<---I give up
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h2[3]').extract_first() #<---I give up
             item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
             item['name'] = link.xpath('a/@title').extract_first()
             item['desc'] = link.xpath('text()').extract_first()
