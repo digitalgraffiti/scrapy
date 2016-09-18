2349eb707bacd47d119ae30f1a0f45ec5581682e
Meg Waring <meg@digitalgraffiti.ca>
2016-09-18 12:35:21 -0700

    I don't know whats wrong.
    i have tried to make it so that the heading it is under is included for each item, but i can't get readable english out of the headers.
    
    sometimes there aren't links and that messes things app
    when there aren't links, the name of what would be a link appears in the description with gibberish characters separatingg then

--- blgenre/genre/genre/items.py
+++ blgenre/genre/genre/items.py
@@ -2,6 +2,9 @@ import scrapy
 
 
 class GenreItem(scrapy.Item):
+    parent = scrapy.Field()
     name = scrapy.Field()
     url = scrapy.Field()
     desc = scrapy.Field()
+
+    // *[ @ id = "mw-content-text"] / h2[3]
--- blgenre/genre/genre/spiders/genre_spider.py
+++ blgenre/genre/genre/spiders/genre_spider.py
@@ -3,16 +3,47 @@ from genre.items import GenreItem
 
 
 class GenreSpider(scrapy.Spider):
-    name = 'genres-wiki'
+    name = 'genre'
     allowed_domains = ['en.wikipedia.org']
     start_urls = ['https://en.wikipedia.org/wiki/List_of_writing_genres']
 
     def parse(self, response):
         # first we get the links from the main genre section
-        links = response.xpath('//*[@id="mw-content-text"]/ul[1]/li/a')
-        for link in links:
+
+# Major genres
+        for link in response.xpath('//*[@id="mw-content-text"]/ul[1]/li/a'):
             item = GenreItem()
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[1]') #<---I give up. I just want the text. this is to be the same on every item in this particular list
             item['url'] = response.urljoin(link.xpath('@href').extract_first())
             item['name'] = link.xpath('@title').extract_first()
             yield item
 
+
+# Common genres: fiction
+        for link in response.xpath('//*[@id="mw-content-text"]/ul[2]/li'):
+            item = GenreItem()
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[2]') #<---I give up
+            item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
+            item['name'] = link.xpath('a/@title').extract_first()# some of these dont have links and it mucks stuff up
+            item['desc'] = link.xpath('text()').extract_first()# the name ends up here with gibberish characters between it and the desc
+            yield item
+
+
+# Common genres: nonfiction
+        for link in response.xpath('//*[@id="mw-content-text"]/ul[3]/li'):
+            item = GenreItem()
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h3[3]') #<---I give up
+            item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
+            item['name'] = link.xpath('a/@title').extract_first()
+            item['desc'] = link.xpath('text()').extract_first()
+            yield item
+
+
+# Genres and subgenres
+        for link in response.xpath('//*[@id="mw-content-text"]/ul[3]/li'):# ther are lots and lots of sub-lists
+            item = GenreItem()
+            item['parent'] = response.xpath('//*[@id="mw-content-text"]/h2[3]') #<---I give up
+            item['url'] = response.urljoin(link.xpath('a/@href').extract_first())
+            item['name'] = link.xpath('a/@title').extract_first()
+            item['desc'] = link.xpath('text()').extract_first()
+            yield item
